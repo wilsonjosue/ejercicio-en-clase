4c237d6aed30cb569bab1b5c30f1f5501fe589ce
Wilson Turpo Huanca <wturpoh@unsa.edu.pe>
2023-05-07 17:07:47 -0500

    agregamos los archivos

---
+++ Bienvenido.java
@@ -0,0 +1,6 @@
+public class Bienvenido {
+
+    public static void main(String[] args) {
+	    System.out.println("Bienvenido al curso de E.D.A");
+    }
+}
---
+++ MiNombre.java
@@ -0,0 +1,7 @@
+public class MiNombre {
+    
+    public static void main(String[] args) {
+        System.out.println("Wilson Josue Turpo Huanca");
+    }
+
+}
